5ce3902e18b65b7ddb1cfa97efbbdc43b4f85250
Gwendoline Delestre <delestregwendoline@gmail.com>
2023-12-11 16:33:53 +0100

    création base

---
+++ insertion_table.sql
@@ -0,0 +1,50 @@
+INSERT INTO Employes(Nom, Prenom, Date_naissance, Email)
+Values ('Bond', 'James','27/12/1995','jamesbond@asso.fr')
+
+INSERT INTO Employes(Nom, Prenom, Date_naissance, Email)
+Values ('Patrick', 'James','27/01/1980','patrick@asso.fr')
+
+INSERT INTO Activites(Sport, Section, Jour, Heure, Capacite_max, Prix, EmployeID)
+Values ('Basketball', 'Adulte Homme','Lundi','08:00:00',18,200,1)
+
+INSERT INTO Activites(Sport, Section, Jour, Heure, Capacite_max, Prix, EmployeID)
+Values ('Basketball', 'Enfant','Mardi','08:00:00',14,150.25,1)
+
+INSERT INTO Activites(Sport, Section, Jour, Heure, Capacite_max, Prix, EmployeID)
+Values ('Volleyball', 'Enfant','Mercredi','08:00:00',14,150.25,1)
+
+INSERT INTO Activites(Sport, Section, Jour, Heure, Capacite_max, Prix, EmployeID)
+Values ('Volleyball', 'Enfant','Jeudi','08:00:00',14,150.25,1)
+
+INSERT INTO Activites(Sport, Section, Jour, Heure, Capacite_max, Prix, EmployeID)
+Values ('Badminton', 'Adulte Homme','Vendredi','08:00:00',18,200,1)
+
+INSERT INTO Activites(Sport, Section, Jour, Heure, Capacite_max, Prix, EmployeID)
+Values ('Badminton', 'Adulte Homme','Samedi','08:00:00',18,200,1)
+
+INSERT INTO Activites(Sport, Section, Jour, Heure, Capacite_max, Prix, EmployeID)
+Values ('Badminton', 'Enfant','Dimanche','08:00:00',14,150.25,1)
+
+INSERT INTO Activites(Sport, Section, Jour, Heure, Capacite_max, Prix, EmployeID)
+Values ('Judo', 'Adulte Homme','Lundi','09:00:00',18,200,1)
+
+INSERT INTO Activites(Sport, Section, Jour, Heure, Capacite_max, Prix, EmployeID)
+Values ('Judo', 'Enfant','Mardi','09:00:00',14,150.25,1)
+
+INSERT INTO Activites(Sport, Section, Jour, Heure, Capacite_max, Prix, EmployeID)
+Values ('Judo', 'Enfant','Mercredi','09:00:00',14,150.25,1)
+
+INSERT INTO Activites(Sport, Section, Jour, Heure, Capacite_max, Prix, EmployeID)
+Values ('Gymnastique', 'Enfant','Jeudi','09:00:00',14,150.25,1)
+
+INSERT INTO Activites(Sport, Section, Jour, Heure, Capacite_max, Prix, EmployeID)
+Values ('Gymnastique', 'Adulte Homme','Vendredi','09:00:00',18,200,1)
+
+INSERT INTO Activites(Sport, Section, Jour, Heure, Capacite_max, Prix, EmployeID)
+Values ('Gymnastique', 'Adulte Homme','Samedi','09:00:00',18,200,1)
+
+INSERT INTO Activites(Sport, Section, Jour, Heure, Capacite_max, Prix, EmployeID)
+Values ('Gymnastique', 'Enfant','Dimanche','09:00:00',14,150.25,1)
+
+Insert into Contact_Urgence(Nom, Prenom,Email,Tel,Relation)
+Values ('Fabien','Thomas', 'fabou@gmail.com',0765858585,'Père')
